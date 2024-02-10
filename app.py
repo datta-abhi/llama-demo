d1b555c23475df3a38af2bba4400a6179a00af0e
datta-abhi <rookbee91@gmail.com>
2024-02-10 11:15:17 +0530

    add end of page to llama page

--- app.py
+++ app.py
@@ -9,3 +9,5 @@ with col2:
     st.write("""
     Llamas, native to the Andean highlands of South America, are captivating creatures that have seamlessly integrated into various human activities. Known for their resilience and amiable nature, llamas serve as reliable pack animals, carrying loads across challenging terrains. With a distinctive appearance, their elongated necks, large expressive eyes, and soft, luxurious coats make them endearing companions. Beyond their charming exterior, llamas are intelligent and responsive, easily trainable for tasks like guarding livestock. The fibers from their coats, prized for their softness and warmth, contribute to textiles. Llamas embody a harmonious blend of utility, companionship, and cultural significance.
     """)
+
+st.subheader('The page ends here')
